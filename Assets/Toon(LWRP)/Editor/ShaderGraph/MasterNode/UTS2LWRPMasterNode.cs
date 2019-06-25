07bb44c2f379986ccb726eb687414def1938e66d
higeometry <toshiyuki.mori.develop@gmail.com>
2019-06-25 21:39:43 +0900

    Implementing UTS2LWRPMasterNode

--- Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPMasterNode.cs
+++ Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPMasterNode.cs
@@ -10,8 +10,40 @@ namespace UTJ.Experimental.UTS2LWRP
 {
     [Serializable]
     [Title("Master", "UTS2 LWRP (Experimental)")]
-    class UTS2LWRPMasterNode : MasterNode<IUTS2SubShader> /*, IMayRequirePosition, IMayRequireNormal*/
+    class UTS2LWRPMasterNode : MasterNode<IUTS2SubShader>, IMayRequirePosition,IMayRequireNormal
     {
+        public NeededCoordinateSpace RequiresNormal(ShaderStageCapability stageCapability = ShaderStageCapability.All)
+        {
+            List<MaterialSlot> slots = new List<MaterialSlot>();
+            GetSlots(slots);
 
+            List<MaterialSlot> validSlots = new List<MaterialSlot>();
+            for (int i = 0; i < slots.Count; i++)
+            {
+                if (slots[i].stageCapability != ShaderStageCapability.All && slots[i].stageCapability != stageCapability)
+                    continue;
+
+                validSlots.Add(slots[i]);
+            }
+            return validSlots.OfType<IMayRequireNormal>().Aggregate(NeededCoordinateSpace.None, (mask, node) => mask | node.RequiresNormal(stageCapability));
+
+        }
+
+        public NeededCoordinateSpace RequiresPosition(ShaderStageCapability stageCapability = ShaderStageCapability.All)
+        {
+            List<MaterialSlot> slots = new List<MaterialSlot>();
+            GetSlots(slots);
+
+            List<MaterialSlot> validSlots = new List<MaterialSlot>();
+            for (int i = 0; i < slots.Count; i++)
+            {
+                if (slots[i].stageCapability != ShaderStageCapability.All && slots[i].stageCapability != stageCapability)
+                    continue;
+
+                validSlots.Add(slots[i]);
+            }
+            return validSlots.OfType<IMayRequirePosition>().Aggregate(NeededCoordinateSpace.None, (mask, node) => mask | node.RequiresPosition(stageCapability));
+
+        }
     }
 }
