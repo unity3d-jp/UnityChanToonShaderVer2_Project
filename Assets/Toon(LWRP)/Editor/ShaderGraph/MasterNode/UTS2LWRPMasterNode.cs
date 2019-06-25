c5bc2a472aa0f1729455ce648f53983d5860d83b
higeometry <toshiyuki.mori.develop@gmail.com>
2019-06-25 21:08:59 +0900

    Created .asmdef file and so on.

---
+++ Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPMasterNode.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditor.Graphing;
+using UnityEngine;
+using UnityEditor.ShaderGraph;
+
+namespace UTJ.Experimental.UTS2LWRP
+{
+    [Serializable]
+    [Title("Master", "Sprite Lit (Experimental)")]
+    public class UTS2LWRPMasterNode : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
