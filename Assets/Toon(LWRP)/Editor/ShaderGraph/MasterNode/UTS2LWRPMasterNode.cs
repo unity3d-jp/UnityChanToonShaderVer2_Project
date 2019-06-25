51ebec3d640f8074039241e9d07f60a1806b7727
higeometry <toshiyuki.mori.develop@gmail.com>
2019-06-25 23:36:35 +0900

    Added IUTS2Shader.cs UTS2LWRPSettingView.cs and UTS2SubShader.cs

--- Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPMasterNode.cs
+++ Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPMasterNode.cs
@@ -5,13 +5,72 @@ using System.Collections.Generic;
 using UnityEditor.Graphing;
 using UnityEngine;
 using UnityEditor.ShaderGraph;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace UTJ.Experimental.UTS2LWRP
 {
     [Serializable]
     [Title("Master", "UTS2 LWRP (Experimental)")]
-    class UTS2LWRPMasterNode : MasterNode<IUTS2SubShader>, IMayRequirePosition,IMayRequireNormal
+    class UTS2LWRPMasterNode : MasterNode<IUTS2Shader>, IMayRequirePosition,IMayRequireNormal
     {
+        public const string AlbedoSlotName = "Albedo";
+        public const string NormalSlotName = "Normal";
+        public const string EmissionSlotName = "Emission";
+        public const string MetallicSlotName = "Metallic";
+        public const string SpecularSlotName = "Specular";
+        public const string SmoothnessSlotName = "Smoothness";
+        public const string OcclusionSlotName = "Occlusion";
+        public const string AlphaSlotName = "Alpha";
+        public const string AlphaClipThresholdSlotName = "AlphaClipThreshold";
+        public const string PositionName = "Position";
+
+        public const int AlbedoSlotId = 0;
+        public const int NormalSlotId = 1;
+        public const int MetallicSlotId = 2;
+        public const int SpecularSlotId = 3;
+        public const int EmissionSlotId = 4;
+        public const int SmoothnessSlotId = 5;
+        public const int OcclusionSlotId = 6;
+        public const int AlphaSlotId = 7;
+        public const int AlphaThresholdSlotId = 8;
+        public const int PositionSlotId = 9;
+
+        [MenuItem("Assets/Create/Shader/UTS2 Graph", false, 208)]
+        public static void CreateUTS2MasterMaterialGraph()
+        {
+            GraphUtil.CreateNewGraph(new UTS2LWRPMasterNode());
+        }
+
+        public UTS2LWRPMasterNode()
+        {
+            UpdateNodeAfterDeserialization();
+        }
+
+        public sealed override void UpdateNodeAfterDeserialization()
+        {
+            base.UpdateNodeAfterDeserialization();
+            name = "UTS2 Master";
+            AddSlot(new PositionMaterialSlot(PositionSlotId, PositionName, PositionName, CoordinateSpace.Object, ShaderStageCapability.Vertex));
+            AddSlot(new ColorRGBMaterialSlot(AlbedoSlotId, AlbedoSlotName, AlbedoSlotName, SlotType.Input, Color.grey.gamma, ColorMode.Default, ShaderStageCapability.Fragment));
+            AddSlot(new NormalMaterialSlot(NormalSlotId, NormalSlotName, NormalSlotName, CoordinateSpace.Tangent, ShaderStageCapability.Fragment));
+
+            // clear out slot names that do not match the slots
+            // we support
+            RemoveSlotsNameNotMatching(
+                new[]
+            {
+                PositionSlotId,
+                AlbedoSlotId,
+                NormalSlotId,
+            }, true);
+        }
+
+        protected override VisualElement CreateCommonSettingsElement()
+        {
+            return new UTS2LWRPSettingsView(this);
+        }
+
         public NeededCoordinateSpace RequiresNormal(ShaderStageCapability stageCapability = ShaderStageCapability.All)
         {
             List<MaterialSlot> slots = new List<MaterialSlot>();
---
+++ Assets/Toon(LWRP)/Editor/ShaderGraph/MasterNode/UTS2LWRPSettingsView.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Graphing.Util;
+using UnityEditor.ShaderGraph.Drawing.Controls;
+using UnityEditor.UIElements;
+using UnityEditor.ShaderGraph.Drawing;
+using UnityEngine.UIElements;
+using UnityEditor.ShaderGraph;
+
+namespace UTJ.Experimental.UTS2LWRP
+{
+    class UTS2LWRPSettingsView : VisualElement
+    {
+        UTS2LWRPMasterNode m_Node;
+        public UTS2LWRPSettingsView(UTS2LWRPMasterNode node)
+        {
+            m_Node = node;
+
+            PropertySheet ps = new PropertySheet();
+
+
+
+            ps.Add(new PropertyRow(new Label("Surface")), (row) =>
+            {
+                row.Add(new EnumField(SurfaceType.Opaque), (field) =>
+                {
+                    field.value = m_Node.surfaceType;
+                    field.RegisterValueChangedCallback(ChangeSurface);
+                });
+            });
+
+            ps.Add(new PropertyRow(new Label("Blend")), (row) =>
+            {
+                row.Add(new EnumField(AlphaMode.Additive), (field) =>
+                {
+                    field.value = m_Node.alphaMode;
+                    field.RegisterValueChangedCallback(ChangeAlphaMode);
+                });
+            });
+
+            ps.Add(new PropertyRow(new Label("Two Sided")), (row) =>
+            {
+                row.Add(new Toggle(), (toggle) =>
+                {
+                    toggle.value = m_Node.twoSided.isOn;
+                    toggle.OnToggleChanged(ChangeTwoSided);
+                });
+            });
+
+            Add(ps);
+
+        }
+
+
+        void ChangeSurface(ChangeEvent<Enum> evt)
+        {
+            if (Equals(m_Node.surfaceType, evt.newValue))
+                return;
+
+            m_Node.owner.owner.RegisterCompleteObjectUndo("Surface Change");
+            m_Node.surfaceType = (SurfaceType)evt.newValue;
+        }
+
+        void ChangeAlphaMode(ChangeEvent<Enum> evt)
+        {
+            if (Equals(m_Node.alphaMode, evt.newValue))
+                return;
+
+            m_Node.owner.owner.RegisterCompleteObjectUndo("Alpha Mode Change");
+            m_Node.alphaMode = (AlphaMode)evt.newValue;
+        }
+
+        void ChangeTwoSided(ChangeEvent<bool> evt)
+        {
+            m_Node.owner.owner.RegisterCompleteObjectUndo("Two Sided Change");
+            ToggleData td = m_Node.twoSided;
+            td.isOn = evt.newValue;
+            m_Node.twoSided = td;
+        }
+
+    }
+
+
+}
+
