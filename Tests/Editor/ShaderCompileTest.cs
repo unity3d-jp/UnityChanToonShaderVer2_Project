911ca046fe197b9bd42250a14def837749e42f6b
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-28 17:10:31 +0900

    Recompile the shader during the test to make sure there is no error compile

--- Tests/Editor/ShaderCompileTest.cs
+++ Tests/Editor/ShaderCompileTest.cs
@@ -1,21 +1,44 @@
 ﻿using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Unity.UnityChanToonShader2.Tests {
     public class ShaderCompileTest
     {
+        
         [Test]
         public void CompileAllToonShaders() {
+            m_shaderCompileError = false;
             string[] guids = AssetDatabase.FindAssets("t:Shader", new[] {"Packages/com.unity.unitychantoonshader2/Runtime/Shader"});
+            Application.logMessageReceived+= ShaderCompileErrorChecker;
             int numShaders = guids.Length;
-            for (int i=0;i<numShaders;++i) {
-                Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(AssetDatabase.GUIDToAssetPath(guids[i]));
-                Assert.True(shader.isSupported);
+
+            for (int i=0;i<numShaders && !m_shaderCompileError;++i) {
+                string curAssetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(curAssetPath);
+
+                AssetDatabase.ImportAsset(curAssetPath); //Recompile the shader to make sure there are no compile errors
+                Assert.True(shader.isSupported);             
                 
             }
 
+            Application.logMessageReceived-= ShaderCompileErrorChecker;
 
+            Assert.False(m_shaderCompileError);
         }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+         static void ShaderCompileErrorChecker(string message, string stackTrace, LogType logType)
+         {
+             // if we receive a Debug.LogError we can assume that compilation failed
+             if (logType == LogType.Error)
+                 m_shaderCompileError  = true;
+         }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+         static bool m_shaderCompileError = false;
     }
 }
