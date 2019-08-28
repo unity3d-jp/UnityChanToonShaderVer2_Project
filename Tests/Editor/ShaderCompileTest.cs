0fc18717138d85815283a1e5b2b0ea86ce2cf1de
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-28 16:32:52 +0900

    Add test files to test if all the shaders can be compiled

---
+++ Tests/Editor/ShaderCompileTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.UnityChanToonShader2.Tests {
+    public class ShaderCompileTest
+    {
+        [Test]
+        public void CompileAllToonShaders() {
+            string[] guids = AssetDatabase.FindAssets("t:Shader", new[] {"Packages/com.unity.unitychantoonshader2/Runtime/Shader"});
+            int numShaders = guids.Length;
+            for (int i=0;i<numShaders;++i) {
+                Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(AssetDatabase.GUIDToAssetPath(guids[i]));
+                Assert.True(shader.isSupported);
+                
+            }
+
+
+        }
+    }
+}
