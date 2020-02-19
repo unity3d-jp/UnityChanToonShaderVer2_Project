0254e96e49fb63801b71d2be70c5fac7083e0510
higeometry <toshiyuki.mori.develop@gmail.com>
2020-02-19 13:28:05 +0900

    Prepared some files to create custom FORWARD Shader.

---
+++ Assets/Toon(HDRP)/Shaders/ShaderPassForwardCopy.hlsl
@@ -0,0 +1,231 @@
+#if SHADERPASS != SHADERPASS_FORWARD
+#error SHADERPASS_is_not_correctly_define
+#endif
+
+#ifdef _WRITE_TRANSPARENT_MOTION_VECTOR
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/MotionVectorVertexShaderCommon.hlsl"
+
+PackedVaryingsType Vert(AttributesMesh inputMesh, AttributesPass inputPass)
+{
+    VaryingsType varyingsType;
+    varyingsType.vmesh = VertMesh(inputMesh);
+    return MotionVectorVS(varyingsType, inputMesh, inputPass);
+}
+
+#ifdef TESSELLATION_ON
+
+PackedVaryingsToPS VertTesselation(VaryingsToDS input)
+{
+    VaryingsToPS output;
+    output.vmesh = VertMeshTesselation(input.vmesh);
+    MotionVectorPositionZBias(output);
+
+    output.vpass.positionCS = input.vpass.positionCS;
+    output.vpass.previousPositionCS = input.vpass.previousPositionCS;
+
+    return PackVaryingsToPS(output);
+}
+
+#endif // TESSELLATION_ON
+
+#else // _WRITE_TRANSPARENT_MOTION_VECTOR
+
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/VertMesh.hlsl"
+
+PackedVaryingsType Vert(AttributesMesh inputMesh)
+{
+    VaryingsType varyingsType;
+    varyingsType.vmesh = VertMesh(inputMesh);
+
+    return PackVaryingsType(varyingsType);
+}
+
+#ifdef TESSELLATION_ON
+
+PackedVaryingsToPS VertTesselation(VaryingsToDS input)
+{
+    VaryingsToPS output;
+    output.vmesh = VertMeshTesselation(input.vmesh);
+
+    return PackVaryingsToPS(output);
+}
+
+
+#endif // TESSELLATION_ON
+
+#endif // _WRITE_TRANSPARENT_MOTION_VECTOR
+
+
+#ifdef TESSELLATION_ON
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/TessellationShare.hlsl"
+#endif
+
+void Frag(PackedVaryingsToPS packedInput,
+#ifdef OUTPUT_SPLIT_LIGHTING
+    out float4 outColor : SV_Target0,  // outSpecularLighting
+    out float4 outDiffuseLighting : SV_Target1,
+    OUTPUT_SSSBUFFER(outSSSBuffer)
+#else
+    out float4 outColor : SV_Target0
+#ifdef _WRITE_TRANSPARENT_MOTION_VECTOR
+    , out float4 outMotionVec : SV_Target1
+#endif // _WRITE_TRANSPARENT_MOTION_VECTOR
+#endif // OUTPUT_SPLIT_LIGHTING
+#ifdef _DEPTHOFFSET_ON
+    , out float outputDepth : SV_Depth
+#endif
+)
+{
+    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(packedInput);
+    FragInputs input = UnpackVaryingsMeshToFragInputs(packedInput.vmesh);
+
+    // We need to readapt the SS position as our screen space positions are for a low res buffer, but we try to access a full res buffer.
+    input.positionSS.xy = _OffScreenRendering > 0 ? (input.positionSS.xy * _OffScreenDownsampleFactor) : input.positionSS.xy;
+
+    uint2 tileIndex = uint2(input.positionSS.xy) / GetTileSize();
+
+    // input.positionSS is SV_Position
+    PositionInputs posInput = GetPositionInput(input.positionSS.xy, _ScreenSize.zw, input.positionSS.z, input.positionSS.w, input.positionRWS.xyz, tileIndex);
+
+#ifdef VARYINGS_NEED_POSITION_WS
+    float3 V = GetWorldSpaceNormalizeViewDir(input.positionRWS);
+#else
+    // Unused
+    float3 V = float3(1.0, 1.0, 1.0); // Avoid the division by 0
+#endif
+
+    SurfaceData surfaceData;
+    BuiltinData builtinData;
+    GetSurfaceAndBuiltinData(input, V, posInput, surfaceData, builtinData);
+
+    BSDFData bsdfData = ConvertSurfaceDataToBSDFData(input.positionSS.xy, surfaceData);
+
+    PreLightData preLightData = GetPreLightData(V, posInput, bsdfData);
+
+    outColor = float4(0.0, 0.0, 0.0, 0.0);
+
+    // We need to skip lighting when doing debug pass because the debug pass is done before lighting so some buffers may not be properly initialized potentially causing crashes on PS4.
+
+#ifdef DEBUG_DISPLAY
+    // Init in debug display mode to quiet warning
+#ifdef OUTPUT_SPLIT_LIGHTING
+    outDiffuseLighting = 0;
+    ENCODE_INTO_SSSBUFFER(surfaceData, posInput.positionSS, outSSSBuffer);
+#endif
+
+    
+
+    // Same code in ShaderPassForwardUnlit.shader
+    // Reminder: _DebugViewMaterialArray[i]
+    //   i==0 -> the size used in the buffer
+    //   i>0  -> the index used (0 value means nothing)
+    // The index stored in this buffer could either be
+    //   - a gBufferIndex (always stored in _DebugViewMaterialArray[1] as only one supported)
+    //   - a property index which is different for each kind of material even if reflecting the same thing (see MaterialSharedProperty)
+    bool viewMaterial = false;
+    int bufferSize = int(_DebugViewMaterialArray[0]);
+    if (bufferSize != 0)
+    {
+        bool needLinearToSRGB = false;
+        float3 result = float3(1.0, 0.0, 1.0);
+
+        // Loop through the whole buffer
+        // Works because GetSurfaceDataDebug will do nothing if the index is not a known one
+        for (int index = 1; index <= bufferSize; index++)
+        {
+            int indexMaterialProperty = int(_DebugViewMaterialArray[index]);
+
+            // skip if not really in use
+            if (indexMaterialProperty != 0)
+            {
+                viewMaterial = true;
+
+                GetPropertiesDataDebug(indexMaterialProperty, result, needLinearToSRGB);
+                GetVaryingsDataDebug(indexMaterialProperty, input, result, needLinearToSRGB);
+                GetBuiltinDataDebug(indexMaterialProperty, builtinData, result, needLinearToSRGB);
+                GetSurfaceDataDebug(indexMaterialProperty, surfaceData, result, needLinearToSRGB);
+                GetBSDFDataDebug(indexMaterialProperty, bsdfData, result, needLinearToSRGB);
+            }
+        }
+
+        // TEMP!
+        // For now, the final blit in the backbuffer performs an sRGB write
+        // So in the meantime we apply the inverse transform to linear data to compensate.
+        if (!needLinearToSRGB)
+            result = SRGBToLinear(max(0, result));
+
+        outColor = float4(result, 1.0);
+    }
+
+    if (!viewMaterial)
+    {
+        if (_DebugFullScreenMode == FULLSCREENDEBUGMODE_VALIDATE_DIFFUSE_COLOR || _DebugFullScreenMode == FULLSCREENDEBUGMODE_VALIDATE_SPECULAR_COLOR)
+        {
+            float3 result = float3(0.0, 0.0, 0.0);
+
+            GetPBRValidatorDebug(surfaceData, result);
+
+            outColor = float4(result, 1.0f);
+        }
+        else if (_DebugFullScreenMode == FULLSCREENDEBUGMODE_TRANSPARENCY_OVERDRAW)
+        {
+            float4 result = _DebugTransparencyOverdrawWeight * float4(TRANSPARENCY_OVERDRAW_COST, TRANSPARENCY_OVERDRAW_COST, TRANSPARENCY_OVERDRAW_COST, TRANSPARENCY_OVERDRAW_A);
+            outColor = result;
+        }
+        else
+#endif
+        {
+#ifdef _SURFACE_TYPE_TRANSPARENT
+            uint featureFlags = LIGHT_FEATURE_MASK_FLAGS_TRANSPARENT;
+#else
+            uint featureFlags = LIGHT_FEATURE_MASK_FLAGS_OPAQUE;
+#endif
+            float3 diffuseLighting;
+            float3 specularLighting;
+
+            LightLoop(V, posInput, preLightData, bsdfData, builtinData, featureFlags, diffuseLighting, specularLighting);
+
+            diffuseLighting *= GetCurrentExposureMultiplier();
+            specularLighting *= GetCurrentExposureMultiplier();
+
+#ifdef OUTPUT_SPLIT_LIGHTING
+            if (_EnableSubsurfaceScattering != 0 && ShouldOutputSplitLighting(bsdfData))
+            {
+                outColor = float4(specularLighting, 1.0);
+                outDiffuseLighting = float4(TagLightingForSSS(diffuseLighting), 1.0);
+            }
+            else
+            {
+                outColor = float4(diffuseLighting + specularLighting, 1.0);
+                outDiffuseLighting = 0;
+            }
+            ENCODE_INTO_SSSBUFFER(surfaceData, posInput.positionSS, outSSSBuffer);
+#else
+            outColor = ApplyBlendMode(diffuseLighting, specularLighting, builtinData.opacity);
+            outColor = EvaluateAtmosphericScattering(posInput, V, outColor);
+#endif
+
+#ifdef _WRITE_TRANSPARENT_MOTION_VECTOR
+            VaryingsPassToPS inputPass = UnpackVaryingsPassToPS(packedInput.vpass);
+            bool forceNoMotion = any(unity_MotionVectorsParams.yw == 0.0);
+            if (forceNoMotion)
+            {
+                outMotionVec = float4(2.0, 0.0, 0.0, 0.0);
+            }
+            else
+            {
+                float2 motionVec = CalculateMotionVector(inputPass.positionCS, inputPass.previousPositionCS);
+                EncodeMotionVector(motionVec * 0.5, outMotionVec);
+                outMotionVec.zw = 1.0;
+            }
+#endif
+        }
+
+#ifdef DEBUG_DISPLAY
+    }
+#endif
+
+#ifdef _DEPTHOFFSET_ON
+    outputDepth = posInput.deviceDepth;
+#endif
+}
