63e8dc5eb1814bf839a0ddc9419717e0b782f290
higeometry <toshiyuki.mori.develop@gmail.com>
2020-03-18 14:52:18 +0900

    Added namespace into UTS_SobelColorEdgeDetection.cs.

--- Assets/UTS_ImageEffect_PPSv2/UTS_SobelColorEdgeDetection.cs
+++ Assets/UTS_ImageEffect_PPSv2/UTS_SobelColorEdgeDetection.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
- 
-[Serializable]
-[PostProcess(typeof(UTS_SobelColorEdgeDetectionRenderer), PostProcessEvent.AfterStack, "UnityChan/UTS_SobelColorEdgeDetection")]
-public sealed class UTS_SobelColorEdgeDetection : PostProcessEffectSettings
+
+namespace UnityEngine.Rendering.Universal.Toon
 {
-    [Header("Edge Detection : Sobel Color Filter")]
-
-    [Tooltip("Edge Color")]
-    public ColorParameter edgesColor = new ColorParameter { value = new Color(0.5f, 0.5f, 0.5f, 1) };
-    [Range(0f, 1f), Tooltip("Color Filter Power")]
-    public FloatParameter filterPower = new FloatParameter { value = 0.3f };
-    [Range(0f, 1f), Tooltip("Filter Threshold")]
-    public FloatParameter threshold = new FloatParameter { value = 0.3f };
-}
-
-
- 
-public sealed class UTS_SobelColorEdgeDetectionRenderer : PostProcessEffectRenderer<UTS_SobelColorEdgeDetection>
-{
-    public override void Render(PostProcessRenderContext context)
+    [Serializable]
+    [PostProcess(typeof(SobelColorEdgeDetectionRenderer), PostProcessEvent.AfterStack, "UnityChan/UTS_SobelColorEdgeDetection")]
+    public sealed class SobelColorEdgeDetection : PostProcessEffectSettings
     {
-        var sheet = context.propertySheets.Get(Shader.Find("Hidden/UnityChan/UTS_SobelColorEdgeDetection"));
-        sheet.properties.SetColor("_EdgesColor", settings.edgesColor);
-        sheet.properties.SetFloat("_FilterPower", settings.filterPower);
-        sheet.properties.SetFloat("_Threshold", settings.threshold);
+        [Header("Edge Detection : Sobel Color Filter")]
 
-        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        [Tooltip("Edge Color")]
+        public UnityEngine.Rendering.PostProcessing.ColorParameter edgesColor = new UnityEngine.Rendering.PostProcessing.ColorParameter { value = new Color(0.5f, 0.5f, 0.5f, 1) };
+        [Range(0f, 1f), Tooltip("Color Filter Power")]
+        public UnityEngine.Rendering.PostProcessing.FloatParameter filterPower = new UnityEngine.Rendering.PostProcessing.FloatParameter { value = 0.3f };
+        [Range(0f, 1f), Tooltip("Filter Threshold")]
+        public UnityEngine.Rendering.PostProcessing.FloatParameter threshold = new UnityEngine.Rendering.PostProcessing.FloatParameter { value = 0.3f };
+    }
+
+
+
+    public sealed class SobelColorEdgeDetectionRenderer : PostProcessEffectRenderer<SobelColorEdgeDetection>
+    {
+        public override void Render(PostProcessRenderContext context)
+        {
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/UnityChan/UTS_SobelColorEdgeDetection"));
+            sheet.properties.SetColor("_EdgesColor", settings.edgesColor);
+            sheet.properties.SetFloat("_FilterPower", settings.filterPower);
+            sheet.properties.SetFloat("_Threshold", settings.threshold);
+
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
     }
 }
