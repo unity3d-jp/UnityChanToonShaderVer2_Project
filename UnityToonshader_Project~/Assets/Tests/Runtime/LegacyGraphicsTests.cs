8099f6e4ce766509445218b8b264a470ae6698e5
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-02-26 19:19:45 +0900

    Adding test scene.

---
+++ UnityToonshader_Project~/Assets/Tests/Runtime/LegacyGraphicsTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+public class LegacyGraphicsTests 
+{
+    [UnityTest, Category("UnityToonLegacy")]
+    public IEnumerator Run(GraphicsTestCase testCase)
+    {
+        // Always wait one frame for scene load
+        yield return null;
+    }
+}
