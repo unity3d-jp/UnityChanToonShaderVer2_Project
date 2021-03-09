100c925e31914c20e8933d13a837c3c1f6c0418c
sindharta <sindharta@gmail.com>
2021-03-09 12:01:32 +0900

    Add ClippingMatteMode and ToonConstants

---
+++ com.unity.toonshader/Runtime/ClippingMatteMode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Unity.Rendering.Toon {
+
+internal enum ClippingMatteMode {
+    None = 0,
+    Base,
+    FirstShade,
+    SecondShade,
+    Highlight,
+    AngelRing,        
+}
+
+
+}
