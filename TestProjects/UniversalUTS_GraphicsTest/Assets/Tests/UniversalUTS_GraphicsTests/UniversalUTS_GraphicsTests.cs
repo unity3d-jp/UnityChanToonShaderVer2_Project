8482bcf486dd9305c8e6c602a88dc5ceb6312aac
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-04-15 04:54:04 +0900

    Fixed: a compile error in the  Universal RP test.

--- TestProjects/UniversalUTS_GraphicsTest/Assets/Tests/UniversalUTS_GraphicsTests/UniversalUTS_GraphicsTests.cs
+++ TestProjects/UniversalUTS_GraphicsTest/Assets/Tests/UniversalUTS_GraphicsTests/UniversalUTS_GraphicsTests.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.XR;
 using UnityEngine.TestTools.Graphics;
-using UnityEditor.TestTools.Graphics
+using UnityEditor.TestTools.Graphics;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.IO;
