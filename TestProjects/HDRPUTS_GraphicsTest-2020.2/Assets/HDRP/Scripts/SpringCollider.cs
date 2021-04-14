9c0062e7f6f4843fa4c067ebb955ed790b603495
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-04-14 13:36:32 +0900

    Created separated HDRP test later than 2020.2.

---
+++ TestProjects/HDRPUTS_GraphicsTest-2020.2/Assets/HDRP/Scripts/SpringCollider.cs
@@ -0,0 +1,25 @@
+﻿//
+//SpringCollider for unity-chan!
+//
+//Original Script is here:
+//ricopin / SpringCollider.cs
+//Rocket Jump : http://rocketjump.skr.jp/unity3d/109/
+//https://twitter.com/ricopin416
+//
+using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.Rendering.Toon.HDRP.Samples
+{
+	public class SpringCollider : MonoBehaviour
+	{
+		//半径
+		public float radius = 0.5f;
+
+		private void OnDrawGizmosSelected ()
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere (transform.position, radius);
+		}
+	}
+}
