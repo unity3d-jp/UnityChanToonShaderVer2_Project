9131885671b1762a4cdda3d9bd63a112a0f2e9ad
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-04-16 18:01:57 +0900

    Added LegacyUTS_GraphicsTest-2020.2

---
+++ TestProjects/LegacyUTS_GraphicsTest-2020.2/Assets/Scenes/UnityChan/Scripts/AutoBlink.cs
@@ -0,0 +1,140 @@
+﻿//
+//AutoBlink.cs
+//Auto Blink Script
+//2014/06/23 N.Kobayashi
+//
+using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.Rendering.Toon.Samples
+{
+	public class AutoBlink : MonoBehaviour
+	{
+
+		public bool isActive = true;				//Activate Auto Blink
+		public SkinnedMeshRenderer ref_SMR_EYE_DEF;	//Ref for EYE_DEF
+		public SkinnedMeshRenderer ref_SMR_EL_DEF;	//Ref for EL_DEF
+		public float ratio_Close = 85.0f;			//Closed Eye blendshape ratio
+		public float ratio_HalfClose = 20.0f;		//Half-Closed Eye blendshape ratio
+		[HideInInspector]
+		public float
+			ratio_Open = 0.0f;
+		private bool timerStarted = false;			//for Start timer
+		private bool isBlink = false;				//For Blink management
+
+		public float timeBlink = 0.4f;				//Time for Blink
+		private float timeRemining = 0.0f;			//Timer Remaining Time
+
+		public float threshold = 0.3f;				//Random Decision Threshold
+		public float interval = 3.0f;				//Random Decision Interval
+
+
+
+		enum Status
+		{
+			Close,
+			HalfClose,
+			Open	//Status of Blink
+		}
+
+
+		private Status eyeStatus;	//Current Blink status
+
+		void Awake ()
+		{
+			//ref_SMR_EYE_DEF = GameObject.Find("EYE_DEF").GetComponent<SkinnedMeshRenderer>();
+			//ref_SMR_EL_DEF = GameObject.Find("EL_DEF").GetComponent<SkinnedMeshRenderer>();
+		}
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			ResetTimer ();
+			// Start a function for random determination
+			StartCoroutine ("RandomChange");
+		}
+
+		//Reset Timer
+		void ResetTimer ()
+		{
+			timeRemining = timeBlink;
+			timerStarted = false;
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+			if (!timerStarted) {
+				eyeStatus = Status.Close;
+				timerStarted = true;
+			}
+			if (timerStarted) {
+				timeRemining -= Time.deltaTime;
+				if (timeRemining <= 0.0f) {
+					eyeStatus = Status.Open;
+					ResetTimer ();
+				} else if (timeRemining <= timeBlink * 0.3f) {
+					eyeStatus = Status.HalfClose;
+				}
+			}
+		}
+
+		void LateUpdate ()
+		{
+			if (isActive) {
+				if (isBlink) {
+					switch (eyeStatus) {
+					case Status.Close:
+						SetCloseEyes ();
+						break;
+					case Status.HalfClose:
+						SetHalfCloseEyes ();
+						break;
+					case Status.Open:
+						SetOpenEyes ();
+						isBlink = false;
+						break;
+					}
+					//Debug.Log(eyeStatus);
+				}
+			}
+		}
+
+		void SetCloseEyes ()
+		{
+			ref_SMR_EYE_DEF.SetBlendShapeWeight (6, ratio_Close);
+			ref_SMR_EL_DEF.SetBlendShapeWeight (6, ratio_Close);
+		}
+
+		void SetHalfCloseEyes ()
+		{
+			ref_SMR_EYE_DEF.SetBlendShapeWeight (6, ratio_HalfClose);
+			ref_SMR_EL_DEF.SetBlendShapeWeight (6, ratio_HalfClose);
+		}
+
+		void SetOpenEyes ()
+		{
+			ref_SMR_EYE_DEF.SetBlendShapeWeight (6, ratio_Open);
+			ref_SMR_EL_DEF.SetBlendShapeWeight (6, ratio_Open);
+		}
+		
+		// Function for Random Decision
+		IEnumerator RandomChange ()
+		{
+			// Start infinite loop
+			while (true) {
+				//Seed generation for random decisions
+				float _seed = Random.Range (0.0f, 1.0f);
+				if (!isBlink) {
+					if (_seed > threshold) {
+						isBlink = true;
+					}
+				}
+				// Put an interval until the next decision
+				yield return new WaitForSeconds (interval);
+			}
+		}
+	}
+}
---
+++ TestProjects/LegacyUTS_GraphicsTest-2020.2/Assets/Scenes/UnityChan/Scripts/RandomWind.cs
@@ -0,0 +1,92 @@
+﻿//
+//RandomWind.cs for unity-chan!
+//
+//Original Script is here:
+//ricopin / RandomWind.cs
+//Rocket Jump : http://rocketjump.skr.jp/unity3d/109/
+//https://twitter.com/ricopin416
+//
+//Modification 2014/12/20
+//Add directional change/gravity influence on the wind.
+//
+
+using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.Rendering.Toon.Samples
+{
+	public class RandomWind : MonoBehaviour
+	{
+		private SpringBone[] springBones;
+		public bool isWindActive = false;
+
+		private bool isMinus = false;				//For wind direction reversal.
+		public float threshold = 0.5f;				// Random Decision Threshold.
+		public float interval = 5.0f;				// Random Decision Interval.
+		public float windPower = 1.0f;				//Wind strength.
+		public float gravity = 0.98f;				//Strength of gravity.
+        public bool isGUI = true;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			springBones = GetComponent<SpringManager> ().springBones;
+			StartCoroutine ("RandomChange");
+		}
+
+
+
+
+
+		// Update is called once per frame
+		void Update ()
+		{
+
+			Vector3 force = Vector3.zero;
+			if (isWindActive) {
+				if(isMinus){
+					force = new Vector3 (Mathf.PerlinNoise (Time.time, 0.0f) * windPower * -0.001f , gravity * -0.001f , 0);
+				}else{
+					force = new Vector3 (Mathf.PerlinNoise (Time.time, 0.0f) * windPower * 0.001f, gravity * -0.001f, 0);
+				}
+
+				for (int i = 0; i < springBones.Length; i++) {
+					springBones [i].springForce = force;
+				}
+			
+			}
+		}
+
+		void OnGUI ()
+		{
+            if (isGUI)
+            {
+                Rect rect1 = new Rect(10, Screen.height - 40, 400, 30);
+                isWindActive = GUI.Toggle(rect1, isWindActive, "Random Wind");
+            }
+		}
+
+		// Function for random decision.
+		IEnumerator RandomChange ()
+		{
+			// Infinite loop start.
+			while (true) {
+				//Seed generation for random decisions.
+				float _seed = Random.Range (0.0f, 1.0f);
+
+				if (_seed > threshold) {
+					//Invert the sign when the _seed is greater than threshold.
+					isMinus = true;
+				}else{
+					isMinus = false;
+				}
+
+				// Put an interval until the next decision.
+				yield return new WaitForSeconds (interval);
+			}
+		}
+
+
+	}
+}
