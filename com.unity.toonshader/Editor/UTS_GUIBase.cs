8935646d97d61f863e1373001530f97615c2f778
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-05-05 07:55:18 +0900

    Updated version number in cs and shader codes.

--- com.unity.toonshader/Editor/UTS_GUIBase.cs
+++ com.unity.toonshader/Editor/UTS_GUIBase.cs
@@ -29,7 +29,7 @@ namespace UnityEditor.Rendering.Toon
     {
         protected const float kVersionX = 0.0f;
         protected const float kVersionY = 0.0f;
-        protected const float kVersionZ = 2.0f;
+        protected const float kVersionZ = 3.0f;
 
         internal virtual string srpDefaultLightModeName { get; }
         internal virtual void TessellationSetting(Material materal) { }
