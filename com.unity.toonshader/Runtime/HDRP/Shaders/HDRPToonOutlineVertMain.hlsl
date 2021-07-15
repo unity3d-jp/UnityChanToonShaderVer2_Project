556ba4e01ad53ba10fe233a4ad48aff2781b59bb
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-07-15 12:54:12 +0900

    reduced warrnings in Unity Chan Toon Shader hsl files.

--- com.unity.toonshader/Runtime/HDRP/Shaders/HDRPToonOutlineVertMain.hlsl
+++ com.unity.toonshader/Runtime/HDRP/Shaders/HDRPToonOutlineVertMain.hlsl
@@ -45,6 +45,6 @@
 #ifndef TESSELLATION_ON
         varyingsType.vmesh.positionCS = clipPos;
 #endif // TESSELLATION_ON
-        varyingsType.vmesh.positionRWS = rws;
+        varyingsType.vmesh.positionRWS = (float3)rws;
 
 #endif // #if 1
