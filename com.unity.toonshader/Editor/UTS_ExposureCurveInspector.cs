94127d2b57c0e9d12a22c5644cd2a38b11cea0e5
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-07-20 18:48:03 +0900

    implementing new exposure system. [skip ci]

--- com.unity.toonshader/Editor/UTS_ExposureCurveInspector.cs
+++ com.unity.toonshader/Editor/UTS_ExposureCurveInspector.cs
@@ -20,87 +20,16 @@ namespace UnityEditor.Rendering.Toon
             const string labelCurveLogic = "Curve Logic";
             const string labelExposureAdjustment = "Exposure Adjustment";
             EditorGUI.BeginChangeCheck();
-            var curveType = EditorGUILayout.Popup(labelCurveLogic, obj.m_CurveType, System.Enum.GetNames(typeof(UTS_ExposureCurveType)));
+
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(target, "Changed UTS Curve Logic");
-                obj.m_CurveType = (int)curveType;
+
                 isChanged = true;
             }
 
 
             Rect rect = GUILayoutUtility.GetRect(Screen.width, 300.0f);
-            EditorGUI.DrawRect(rect, Color.black);
-            const int zoomMax = 10;
-            float yMin = -zoomMax;
-            float yMax = zoomMax;
-            float xMin = -0.5f;
-            float xMax = zoomMax;
-  
-            const float kTwo = 2.0f;
-            const float kOne = 1.0f;
-            const float kZero = 0.0f;
-            const float kDashSize = 2.0f;
-            Handles.color = Color.green;
-
-            float step = 1 / 1000.0f;
-            Vector3 prevPos = new Vector3(0, curveFunc(0.0001f, (UTS_ExposureCurveType)curveType), 0);
-            for (float t = step; t < 10.0  +step ; t += step)
-            {
-                Vector3 pos = new Vector3(t, curveFunc(t, (UTS_ExposureCurveType)curveType), 0);
-                Handles.DrawLine(
-                    new Vector3(rect.xMin + (prevPos.x - xMin) / (xMax - xMin)  * rect.width /zoom, rect.yMax - ((prevPos.y - yMin) / (yMax - yMin)) * rect.height/zoom, 0),
-                    new Vector3(rect.xMin + (pos.x - xMin) / (xMax - xMin)  * rect.width/zoom, rect.yMax - ((pos.y - yMin) / (yMax - yMin)) * rect.height/zoom, 0));
-
-                prevPos = pos;
-                if ( t > 1.0f )
-                {
-                    step = 1.0f;
-                }
-
-            }
-            Handles.color = Color.white;
-            // y == 0.0f;
-            Handles.DrawLine(
-                    new Vector3(rect.xMin, rect.yMax - ((kZero - yMin) / (yMax - yMin)) * rect.height / zoom, 0),
-                    new Vector3(rect.xMax, rect.yMax - ((kZero - yMin) / (yMax - yMin)) * rect.height / zoom, 0));
-            // x == 0.0f;
-            Handles.DrawLine(
-                   new Vector3(rect.xMin + (kZero - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMin, 0),
-                   new Vector3(rect.xMin + (kZero - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMax, 0));
-            Handles.color = Color.gray;
-
-            // y == 2.0f;
-            Handles.DrawDottedLine(
-                    new Vector3(rect.xMin, rect.yMax - ((kTwo - yMin) / (yMax - yMin)) * rect.height / zoom, 0),
-                    new Vector3(rect.xMax, rect.yMax - ((kTwo - yMin) / (yMax - yMin)) * rect.height / zoom, 0), kDashSize);
-            // y == 1.0f;
-            Handles.DrawDottedLine(
-                    new Vector3(rect.xMin, rect.yMax - ((kOne - yMin) / (yMax - yMin)) * rect.height / zoom, 0),
-                    new Vector3(rect.xMax, rect.yMax - ((kOne - yMin) / (yMax - yMin)) * rect.height / zoom, 0), kDashSize);
-
-            // x == 1.0f;
-            Handles.DrawDottedLine(
-                   new Vector3(rect.xMin + (kTwo - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMin, 0),
-                   new Vector3(rect.xMin + (kTwo - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMax, 0), kDashSize);
-            // x == 1.0f;
-            Handles.DrawDottedLine(
-                   new Vector3(rect.xMin + (kOne - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMin, 0),
-                   new Vector3(rect.xMin + (kOne - xMin) / (xMax - xMin) * rect.width / zoom, rect.yMax, 0), kDashSize);
-
-            Handles.color = Color.white;
-
-
-            zoom = EditorGUILayout.Slider(zoom, 1, zoomMax);
-
-            Handles.Label(new Vector3(rect.xMin + (kTwo - xMin) / (xMax - xMin) * rect.width / zoom - 20.0f, rect.yMax - ((2.0f - yMin) / (yMax - yMin)) * rect.height / zoom - 12.0f, 0),
-                kTwo.ToString());
-            Handles.Label(new Vector3(rect.xMin + (kOne - xMin) / (xMax - xMin) * rect.width / zoom - 20.0f, rect.yMax - ((1.0f - yMin) / (yMax - yMin)) * rect.height/zoom - 12.0f,0),
-                kOne.ToString() );
-
-            Handles.Label(new Vector3(rect.xMin + (kZero - xMin) / (xMax - xMin) * rect.width / zoom - 20.0f, rect.yMax - ((0.0f - yMin) / (yMax - yMin)) * rect.height / zoom - 16.0f, 0),
-                kZero.ToString());
-
 
             EditorGUI.BeginChangeCheck();
             bool exposureAdjustment = EditorGUILayout.Toggle(labelExposureAdjustment, obj.m_expssureAdjustmnt);
@@ -124,33 +53,8 @@ namespace UnityEditor.Rendering.Toon
                 // at leaset 2020.3.12f1, not neccessary. but, from which version??
                 EditorApplication.QueuePlayerLoopUpdate();
             }
-            //            Handles.DrawSolidDisc(area.center, Vector3.forward, 80f);
+
         }
-        float curveFunc(float t, UTS_ExposureCurveType curveType)
-        {
 
-            float result = 0.0f;
-            switch (curveType)
-            {
-                case UTS_ExposureCurveType.Linear:
-                    result =  t;
-                    break;
-                case UTS_ExposureCurveType.Log:
-                    result = Mathf.Log(t);
-
-                    break;
-                case UTS_ExposureCurveType.Log2:
-                    result = Mathf.Log(t, 2.0f);
-
-                    break;
-                case UTS_ExposureCurveType.Log10:
-                    result = Mathf.Log10(t);
-
-                    break;
-                default:
-                    break;
-            }
-            return result;
-        }
     }
 }
--- com.unity.toonshader/Runtime/UTS_ExposureCurve.cs
+++ com.unity.toonshader/Runtime/UTS_ExposureCurve.cs
@@ -1,52 +1,144 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+using UnityObject = UnityEngine.Object;
 namespace Unity.Rendering.Toon
 {
     [ExecuteAlways]
     [DisallowMultipleComponent]
     public class UTS_ExposureCurve : MonoBehaviour
     {
-        const string kExposureCurvePropName = "_ExposureCurveType";
+
+        const int k_ExposureCurvePrecision = 128;
+        Texture2D m_ExposureCurveTexture;
         const string kExposureAdjustmentPropName = "_ExposureAdjustment";
-        //      const string kLinearFrom0To1Name = "_LinearFrom0to10";
-        //      const string kExopsureMultiplier = "_ExopsureMultiplier";
-        [SerializeField]
-        public int m_CurveType;
+
         [SerializeField]
         public bool m_expssureAdjustmnt = true;
-	[SerializeField]
-	public float m_ExopsureMultiplier = 0.0f;
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField]
+        public float m_ExopsureMultiplier = 0.0f;
+        bool m_initialized = false;
+        bool m_srpCallbackInitialized = false;
+#if UNITY_EDITOR
+#pragma warning restore CS0414
+        bool m_isCompiling = false;
+#endif
+
+
+
+        void Update()
         {
+
+            Initialize();
+
+            Shader.SetGlobalInt(kExposureAdjustmentPropName, m_expssureAdjustmnt ? 1 : 0);
+
+#if UNITY_EDITOR
+            // handle script recompile
+            if (EditorApplication.isCompiling && !m_isCompiling)
+            {
+                // on compile begin
+                m_isCompiling = true;
+                Release();
+            }
+            else if (!EditorApplication.isCompiling && m_isCompiling)
+            {
+                // on compile end
+                m_isCompiling = false;
+            }
+#endif
 
         }
 
-        // Update is called once per frame
-        void Update()
+        void EnableSrpCallbacks()
         {
-            const int logMax = 4;
-            if ( m_CurveType >= logMax)
+
+            if (!m_srpCallbackInitialized)
             {
-                m_CurveType = logMax - 1;
+                m_srpCallbackInitialized = true;
             }
-            if ( m_CurveType < 0 )
+        }
+        void DisableSrpCallbacks()
+        {
+            if (m_srpCallbackInitialized)
             {
-                m_CurveType = 0;
+                m_srpCallbackInitialized = false;
             }
-            Shader.SetGlobalInt(kExposureCurvePropName, m_CurveType);
-           Shader.SetGlobalInt(kExposureAdjustmentPropName, m_expssureAdjustmnt ? 1 : 0);
-//	        Shader.SetGlobalFloat(kExopsureMultiplier, m_ExopsureMultiplier);
+        }
+
+        void OnEnable()
+        {
+
+            Initialize();
+
+            EnableSrpCallbacks();
+
+        }
+
+        void OnDisable()
+        {
+            DisableSrpCallbacks();
+
+            Release();
+        }
+
+        void Initialize()
+        {
+            if (m_initialized)
+            {
+                return;
+            }
+#if UNITY_EDITOR
+            // initializing renderer can interfere GI baking. so wait until it is completed.
+
+            if (EditorApplication.isCompiling)
+                return;
+#endif
+            Debug.Assert(m_ExposureCurveTexture == null);
+            m_ExposureCurveTexture = new Texture2D(k_ExposureCurvePrecision, 1, TextureFormat.RHalf, false, true)
+            {
+                name = "Exposure Curve",
+                filterMode = FilterMode.Bilinear,
+                wrapMode = TextureWrapMode.Clamp
+            };
+
+
+            m_initialized = true;
+        }
+
+
+        void Release()
+        {
+            if (m_initialized)
+            {
+                 DestroyUnityObject(m_ExposureCurveTexture);
+                 m_ExposureCurveTexture = null;
+            }
+
+            m_initialized = false;
+
+        }
+
+        public static void DestroyUnityObject(UnityObject obj)
+        {
+            if (obj != null)
+            {
+#if UNITY_EDITOR
+                if (Application.isPlaying)
+                    UnityObject.Destroy(obj);
+                else
+                    UnityObject.DestroyImmediate(obj);
+#else
+                UnityObject.Destroy(obj);
+#endif
+            }
         }
     }
 
-    public enum UTS_ExposureCurveType
-    {
-        Linear,
-        Log,
-        Log2,
-        Log10
-    };
 
 }
