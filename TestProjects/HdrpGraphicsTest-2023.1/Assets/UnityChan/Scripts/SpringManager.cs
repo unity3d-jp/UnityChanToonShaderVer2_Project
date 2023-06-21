ea4b2e20679f32f3b8f3fead5251836b4d6d6914
H3idi-X <toshiyuki@unity3d.com>
2023-06-21 15:59:37 +0900

    Task/2023.1 hdrp2 (#318)
    * Added HDRP 2023 test.
    
    * Deleted osx_metal-hdrp-2023.1.yml as it doesn't work on yamato.

---
+++ TestProjects/HdrpGraphicsTest-2023.1/Assets/UnityChan/Scripts/SpringManager.cs
@@ -0,0 +1,86 @@
+﻿//
+//SpingManager.cs for unity-chan!
+//
+//Original Script is here:
+//ricopin / SpingManager.cs
+//Rocket Jump : http://rocketjump.skr.jp/unity3d/109/
+//https://twitter.com/ricopin416
+//
+//Revised by N.Kobayashi 2014/06/24
+//           Y.Ebata
+//
+using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.Rendering.Toon.HDRP.Samples
+{
+	public class SpringManager : MonoBehaviour
+	{
+		//Kobayashi
+		// DynamicRatio is paramater for activated level of dynamic animation 
+		public float dynamicRatio = 1.0f;
+
+		//Ebata
+		public float			stiffnessForce;
+		public AnimationCurve	stiffnessCurve;
+		public float			dragForce;
+		public AnimationCurve	dragCurve;
+		public SpringBone[] springBones;
+
+		void Start ()
+		{
+			UpdateParameters ();
+		}
+
+#if UNITY_EDITOR
+		void Update ()
+		{
+
+		//Kobayashi
+		if(dynamicRatio >= 1.0f)
+			dynamicRatio = 1.0f;
+		else if(dynamicRatio <= 0.0f)
+			dynamicRatio = 0.0f;
+		//Ebata
+		UpdateParameters();
+
+		}
+#endif	
+		private void LateUpdate ()
+		{
+			//Kobayashi
+			if (dynamicRatio != 0.0f) {
+				for (int i = 0; i < springBones.Length; i++) {
+					if (dynamicRatio > springBones [i].threshold) {
+						springBones [i].UpdateSpring ();
+					}
+				}
+			}
+		}
+
+		private void UpdateParameters ()
+		{
+			UpdateParameter ("stiffnessForce", stiffnessForce, stiffnessCurve);
+			UpdateParameter ("dragForce", dragForce, dragCurve);
+		}
+	
+		private void UpdateParameter (string fieldName, float baseValue, AnimationCurve curve)
+		{
+			#if UNITY_EDITOR
+			var start = curve.keys [0].time;
+			var end = curve.keys [curve.length - 1].time;
+			//var step	= (end - start) / (springBones.Length - 1);
+		
+			var prop = springBones [0].GetType ().GetField (fieldName, System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+		
+			for (int i = 0; i < springBones.Length; i++) {
+				//Kobayashi
+				if (!springBones [i].isUseEachBoneForceSettings) {
+					var scale = curve.Evaluate (start + (end - start) * i / (springBones.Length - 1));
+					prop.SetValue (springBones [i], baseValue * scale);
+				}
+			}
+			#endif
+		}
+	}
+}
